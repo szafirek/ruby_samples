5eee041ec95cf002cf17ac4ebc9ef3877dd9ccac
Justyna Mianowska <justyna.mianowska90@gmail.com>
2015-12-06 14:38:22 +0100

    Add two sorting algorithms

--- algorithms/heap_sort.rb
+++ algorithms/heap_sort.rb
@@ -1,1 +1,34 @@
 #!/usr/bin/env ruby -w
+
+def heap_sort(a)
+  build_heap(a)
+  (a.size - 1).downto(1) do |i|
+    a[0], a[i] = a[i], a[0]
+    heapify(a, i, 0)
+  end
+  a
+end
+
+def build_heap(a)
+  (a.size / 2 - 1).downto(0) do |i|
+    heapify(a, a.size, i)
+  end
+  a
+end
+
+def heapify(a, size, i)
+  left = 2*i + 1
+  right = 2*i + 2
+  if left < size && a[left] > a[i]
+    max = left
+  else
+    max = i
+  end
+  if right < size && a[right] > a[max]
+    max = right
+  end
+  if max != i
+    a[i], a[max] = a[max], a[i]
+    heapify(a, size, max)
+  end
+end
--- algorithms/quick_sort.rb
+++ algorithms/quick_sort.rb
@@ -1,1 +1,8 @@
 #!/usr/bin/env ruby -w
+
+def quick_sort(a)
+  return a if a.length <= 1
+  pivot = a.pop
+  left, right = a.partition { |x| x < pivot }
+  quick_sort(left) + [pivot] + quick_sort(right)
+end
